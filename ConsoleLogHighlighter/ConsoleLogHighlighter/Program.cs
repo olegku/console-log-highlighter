79ea873e12cf8dbe5f31554e36351a106e9ed114
olegku <kukumber@gmail.com>
2021-04-26 02:25:03 +0300

    Init solution

---
+++ ConsoleLogHighlighter/ConsoleLogHighlighter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleLogHighlighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
